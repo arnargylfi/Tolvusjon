a2f862ded1b555345ea7f10f93852905369ce8b2
Arnar Gylfi <arnargylfi01@gmail.com>
2024-11-25 15:27:59 +0000

    Mostly complete

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -13,23 +13,20 @@ while(True):
     
     # Mark the brightest spot on the original frame
 
-    cv2.drawMarker(frame, brightest_loc, (0, 0, 255), markerType=cv2.MARKER_DIAMOND, thickness=1)
+    cv2.circle(frame, brightest_loc,10, (255, 0, 0), thickness=1)
     #Brightest spot text
-    cv2.putText(frame, f"Brightest", (brightest_loc[0]-40,brightest_loc[1]+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    cv2.putText(frame, f"Brightest", (brightest_loc[0]-40,brightest_loc[1]+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
     # Intensity values for all colors
-    red_channel = frame[:, :, 2]
-    green_channel = frame[:, :, 1]
-    blue_channel = frame[:, :, 0]
-
+    b,g,r = cv2.split(frame)
     # Find reddest spot by finding max (red/blue+green) plus small number in denominator to avoid division with 0 
-    color_ratio = red_channel/(green_channel + blue_channel+1e-5)
+    color_ratio = r/(g + b)
     color_ratio = color_ratio.astype(np.uint8)
 
     _, _, _, reddest_loc = cv2.minMaxLoc(color_ratio)
 
-    cv2.drawMarker(frame, reddest_loc, (0, 0, 255), markerType=cv2.MARKER_TRIANGLE_UP, thickness=2)
-    cv2.putText(frame, "Reddest", (reddest_loc[0] - 40, reddest_loc[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.circle(frame, reddest_loc,10, (0, 0, 255), 1)
+    cv2.putText(frame, "Reddest", (reddest_loc[0] - 40, reddest_loc[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
 
 
 
