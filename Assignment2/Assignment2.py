127523aefb03f3fe9fe7c6986a7485ea8da65f53
Arnar Gylfi <arnargylfi01@gmail.com>
2024-11-26 18:04:11 +0000

    assignment2 nógu gott

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
+import time
+
 
 #Ofanvarp formula til að reikna frá punkti til línu
 def ofanvarp(x1,y1,slope,y_intercept):
@@ -20,7 +22,7 @@ def f(delta,N=None,p=None, e = None, s = None):
         #capture frame 
         _, frame = cap.read()   
         #grab edges with Canny
-        edges = cv2.Canny(frame,50,150)
+        edges = cv2.Canny(frame,100,200)
         #remove zero values to grab edge pixel coordinates
         edge_array = np.column_stack(np.where(edges != 0))
         #RANSAC
@@ -35,6 +37,7 @@ def f(delta,N=None,p=None, e = None, s = None):
             random_points = edge_array[np.random.choice(edge_array.shape[0], N, replace=False)]
             iterations = N
         #Iterate throught the points
+        print(random_points.shape)
         for i in range(iterations-1):
             pkt1 = random_points[i,:]
             pkt2 = random_points[i+1,:]
@@ -49,12 +52,13 @@ def f(delta,N=None,p=None, e = None, s = None):
             if count_within_delta > best_count:
                 best_count = count_within_delta
                 bestpoints = np.array([pkt1,pkt2])
-        cv2.line(frame, (bestpoints[0,0],bestpoints[0,1]), (bestpoints[1,0],bestpoints[1,1]), (0, 0, 255), 2)
-        print(bestpoints)
+        cv2.line(frame, (bestpoints[0,1],bestpoints[0,0]), (bestpoints[1,1],bestpoints[1,0]), (0, 0, 255), 2)
+        for point in random_points:
+            cv2.circle(frame,(point[1],point[0]),2,(0,0,255),-1)
         cv2.imshow('Frame',frame)
         cv2.imshow('edges',edges)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-f(5,100)
+f(5,50)
 cap.release()
 cv2.destroyAllWindows()
